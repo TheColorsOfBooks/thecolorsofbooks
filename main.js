ff3b23b5d8f8ddc6ed0a46b6343d8f1179ff9cff
Caroline Thordenberg <caroline.thordenberg@gmail.com>
2015-01-29 01:26:27 +0100

    final touchez

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        }, 5000, 'easeInOutExpo');
         event.preventDefault();
     });
 });
